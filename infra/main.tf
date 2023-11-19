870eb01172d26d3b97b982d907cb651c53c12f83
Nikola <nikola.kuzman@quietplatforms.com>
2023-11-19 22:58:45 +0100

    Infra change

--- infra/main.tf
+++ infra/main.tf
@@ -10,35 +10,112 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "nkuzman-vpc"
+# Dev Environment
+resource "google_compute_network" "dev_vpc_network" {
+  name = "nkuzman-dev-vpc"
 }
 
-resource "google_compute_subnetwork" "public_subnet" {
-  name          = var.public_subnet
+resource "google_compute_subnetwork" "dev_public_subnet" {
+  name          = var.dev_public_subnet
   ip_cidr_range = "10.0.1.0/24"
   region        = var.region
-  network       = google_compute_network.vpc_network.name
+  network       = google_compute_network.dev_vpc_network.name
 
   private_ip_google_access = false
 }
 
-resource "google_compute_subnetwork" "private_subnet" {
-  name          = var.private_subnet
+resource "google_compute_subnetwork" "dev_private_subnet" {
+  name          = var.dev_private_subnet
   ip_cidr_range = "10.0.2.0/24"
   region        = var.region
-  network       = google_compute_network.vpc_network.name
+  network       = google_compute_network.dev_vpc_network.name
 
   private_ip_google_access = true
 }
 
-resource "google_container_cluster" "my_cluster" {
-  name     = "nkuzman"
+resource "google_container_cluster" "dev_cluster" {
+  name     = "nkuzman-dev"
   location = var.region
   project  = var.project_id
 
   node_pool {
-    name       = "default-node-pool"
+    name       = "dev-node-pool"
+    node_count = 1
+
+    node_config {
+      machine_type = "n1-standard-2"
+    }
+  }
+}
+
+# Staging Environment
+resource "google_compute_network" "staging_vpc_network" {
+  name = "nkuzman-staging-vpc"
+}
+
+resource "google_compute_subnetwork" "staging_public_subnet" {
+  name          = var.staging_public_subnet
+  ip_cidr_range = "10.0.3.0/24"
+  region        = var.region
+  network       = google_compute_network.staging_vpc_network.name
+
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "staging_private_subnet" {
+  name          = var.staging_private_subnet
+  ip_cidr_range = "10.0.4.0/24"
+  region        = var.region
+  network       = google_compute_network.staging_vpc_network.name
+
+  private_ip_google_access = true
+}
+
+resource "google_container_cluster" "staging_cluster" {
+  name     = "nkuzman-staging"
+  location = var.region
+  project  = var.project_id
+
+  node_pool {
+    name       = "staging-node-pool"
+    node_count = 1
+
+    node_config {
+      machine_type = "n1-standard-2"
+    }
+  }
+}
+
+# Production Environment
+resource "google_compute_network" "prod_vpc_network" {
+  name = "nkuzman-prod-vpc"
+}
+
+resource "google_compute_subnetwork" "prod_public_subnet" {
+  name          = var.prod_public_subnet
+  ip_cidr_range = "10.0.5.0/24"
+  region        = var.region
+  network       = google_compute_network.prod_vpc_network.name
+
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "prod_private_subnet" {
+  name          = var.prod_private_subnet
+  ip_cidr_range = "10.0.6.0/24"
+  region        = var.region
+  network       = google_compute_network.prod_vpc_network.name
+
+  private_ip_google_access = true
+}
+
+resource "google_container_cluster" "prod_cluster" {
+  name     = "nkuzman-prod"
+  location = var.region
+  project  = var.project_id
+
+  node_pool {
+    name       = "prod-node-pool"
     node_count = 1
 
     node_config {
--- infra/variables.tf
+++ infra/variables.tf
@@ -10,14 +10,38 @@ variable "region" {
   description = "Region for project"
 }
 
-variable "public_subnet" {
+variable "dev_public_subnet" {
   type        = string
-  default     = "nkuzman-public-subnet"
-  description = "Public subnet"
+  default     = "dev-public-subnet"
+  description = "Dev Public subnet"
 }
 
-variable "private_subnet" {
+variable "dev_private_subnet" {
   type        = string
-  default     = "nkuzman-private-subnet"
-  description = "Private subnet"
+  default     = "dev-private-subnet"
+  description = "Dev Private subnet"
+}
+
+variable "staging_public_subnet" {
+  type        = string
+  default     = "staging-public-subnet"
+  description = "Staging Public subnet"
+}
+
+variable "staging_private_subnet" {
+  type        = string
+  default     = "staging-private-subnet"
+  description = "Staging Private subnet"
+}
+
+variable "prod_public_subnet" {
+  type        = string
+  default     = "prod-public-subnet"
+  description = "Prod Public subnet"
+}
+
+variable "prod_private_subnet" {
+  type        = string
+  default     = "prod-private-subnet"
+  description = "Prod Private subnet"
 }
